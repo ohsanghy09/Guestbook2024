2fd82f8d46179b88780fa8c3d3dc58d4ab39ac71
ohsanghy09 <ohsanghy09@naver.com>
2024-05-23 11:39:01 +0900

    2024-05-23 list.html th를 활용한 view 계층 설정

---
+++ src/main/java/kr/ac/kopo/guestbook2024/controller/GuestbookController.java
@@ -0,0 +1,30 @@
+package kr.ac.kopo.guestbook2024.controller;
+
+
+import kr.ac.kopo.guestbook2024.dto.PageRequestDTO;
+import kr.ac.kopo.guestbook2024.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/guestbook")
+@RequiredArgsConstructor
+public class GuestbookController {
+    private final GuestbookService service;
+
+    @GetMapping("/")
+    public String index(){
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+        //list.html(View계층)에 방명록 목록과 화면에 보여질때 필요한 페이지 번호 등의 정보 전달
+        model.addAttribute("result", service.getList(pageRequestDTO));
+    }
+
+
+} //main}
