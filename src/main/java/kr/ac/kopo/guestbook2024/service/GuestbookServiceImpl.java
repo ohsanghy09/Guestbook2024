d1f38818b5d84d258d8f6f6a48fe62835351c03d
ohsanghy09 <ohsanghy09@naver.com>
2024-06-20 10:27:24 +0900

    2024-06-20 서버측 검색 처리를 위한 정보(type, keyword) 서비스 계층 코드 추가

--- src/main/java/kr/ac/kopo/guestbook2024/service/GuestbookServiceImpl.java
+++ src/main/java/kr/ac/kopo/guestbook2024/service/GuestbookServiceImpl.java
@@ -1,9 +1,12 @@
 package kr.ac.kopo.guestbook2024.service;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import kr.ac.kopo.guestbook2024.dto.GuestbookDTO;
 import kr.ac.kopo.guestbook2024.dto.PageRequestDTO;
 import kr.ac.kopo.guestbook2024.dto.PageResultDTO;
 import kr.ac.kopo.guestbook2024.entity.Guestbook;
+import kr.ac.kopo.guestbook2024.entity.QGuestbook;
 import kr.ac.kopo.guestbook2024.repository.GuestbookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -34,7 +37,9 @@ public class GuestbookServiceImpl implements  GuestbookService{
     @Override
     public PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO) {
         Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending()); //내림차순 정렬
-        Page<Guestbook> result = repository.findAll(pageable);
+        // 검색 기능 추가
+        BooleanBuilder booleanBuilder = getSearch(requestDTO); // where절의 조건식
+        Page<Guestbook> result = repository.findAll(booleanBuilder, pageable); // 조건식이 포함된 select문 생성
         Function<Guestbook, GuestbookDTO> fn = (entity -> entityToDto(entity));
         return new PageResultDTO<>(result, fn);
     }
@@ -60,6 +65,42 @@ public class GuestbookServiceImpl implements  GuestbookService{
     @Override
     public void remove(Long gno) {
         repository.deleteById(gno);
+    }
+
+    @Override
+    public BooleanBuilder getSearch(PageRequestDTO requestDTO) {
+        String type = requestDTO.getType();
+        String keyword = requestDTO.getKeyword();
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+        BooleanExpression booleanExpression = qGuestbook.gno.gt(0L);
+
+        booleanBuilder.and(booleanExpression);
+
+        // 화면에서 검색 조건을 선택하지 않은 경우 검색 타입 = null 및 검색어는 입력이 안된 경우
+        if(type == null || keyword.trim().length()==0 || type.trim().length()==0){
+            return booleanBuilder;
+        }
+
+        // 검색 조건 작성
+        BooleanBuilder conditionBuilder = new BooleanBuilder();
+        if (type.contains("t")){
+            conditionBuilder.or(qGuestbook.title.contains(keyword));
+        }
+
+        if (type.contains("c")){
+            conditionBuilder.or(qGuestbook.content.contains(keyword));
+        }
+
+        if (type.contains("w")){
+            conditionBuilder.or(qGuestbook.writer.contains(keyword));
+        }
+
+        booleanBuilder.and(conditionBuilder);
+
+
+        return booleanBuilder;
     }
 
 
